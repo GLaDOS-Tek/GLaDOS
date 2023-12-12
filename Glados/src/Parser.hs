7dfe8933a115e950e33be68fe2cbd9b74565073b
Skaahn <stanislas.commenge@epitech.eu>
2023-12-12 15:23:16 +0100

    chore: clean code

--- Glados/src/Parser.hs
+++ Glados/src/Parser.hs
@@ -72,7 +72,7 @@ parsePair _ _ = Nothing
 
 readUInt :: String -> String -> Maybe (Int, String)
 readUInt s xs = case (readMaybe s :: Maybe Int) of
-        Just nb -> trace ("Success[readUInt] : `" ++ show nb ++ "`") Just (nb, xs)
+        Just nb -> Just (nb, xs)
         Nothing -> Nothing
 
 parseUInt :: Parser Int -- parse an unsigned Int
@@ -80,14 +80,14 @@ parseUInt s = case (parseSome(parseAnyChar ['0'..'9']) s) of
     Just (x, (' ':xs)) -> readUInt x (' ':xs)
     Just (x, (')':xs)) -> readUInt x (')':xs)
     Just (x, "") -> readUInt x ""
-    _ -> trace ("Fail[parseUInt] : not a uint") Nothing
+    _ -> Nothing
     where
         parsed = parseSome (parseAnyChar ['0'..'9']) s
 
 parseInt :: Parser Int -- parse a signed Int
 parseInt ('-':xs) = case parseUInt xs of
-    Just (a, b) -> trace ("Success[parseInt] : `-" ++ show a ++ "`") Just ((-a), b)
-    Nothing -> trace ("Fail[parseInt] : not an neg int") Nothing
+    Just (a, b) -> Just ((-a), b)
+    Nothing -> Nothing
 parseInt s = parseUInt s
 
 getBiggerLine :: Int -> SExpr -> Int
@@ -98,39 +98,37 @@ getBiggerLine line (ExprList _) = line
 
 parseListElement :: Int -> Parser [SExpr]
 parseListElement line (' ':xs) = parseListElement line xs --prevent to give " )" to general parser
-parseListElement line (')':xs) = trace ("Info[parseListElement] : end of list") Just ([], xs)
+parseListElement line (')':xs) = Just ([], xs)
 parseListElement line str = case generalParser line str of
     Just (a, (')':as)) -> Just ([a], as)
     Just (a, as) -> case parseListElement (getBiggerLine line a) as of
         Just (b, bs) -> Just ([a] ++ b, bs)
         Nothing -> Nothing
-    _ -> trace ("Error[parseListElement] : general parser failed") Nothing
+    _ -> Nothing
 
 parseList :: Int -> Parser [SExpr]
-parseList line ('(':xs) = trace ("Success : that's a list") parseListElement line xs
-parseList _ s = trace ("Fail[parseList] : not a list -> `" ++ [head s] ++ "`" ) Nothing
+parseList line ('(':xs) = parseListElement line xs
+parseList _ s = Nothing
 
 parseString :: Int -> Parser SExpr
-parseString l "" = trace ("Fail[parseString] : empty string") Nothing
-parseString l (' ':xs) = trace ("Fail[parseString]: begin with space") Nothing
+parseString l "" = Nothing
+parseString l (' ':xs) = Nothing
 parseString l ('\"':xs) = case parseSome (parseAnyChar (filter (/= '\"') ascii_char)) xs of
-    Just (a, '\"':as) -> trace ("Success[parseString] : string : `" ++ show a ++ "`") Just (StrExpr a l, as)
-    _ -> trace ("Fail[parseString] : no closing comma") Nothing
+    Just (a, '\"':as) -> Just (StrExpr a l, as)
+    _ -> Nothing
 parseString l s = case parseSome (parseAnyChar valid_char) s of
-    Just (a, as) -> trace ("Success[parseString] : symbol : `" ++ show a ++ "`") Just (SymbolExpr a l, as)
-    _ -> trace ("Fail[parseString] : not a valid symbol : `" ++ [head s] ++ "`") Nothing
+    Just (a, as) -> Just (SymbolExpr a l, as)
+    _ -> Nothing
 
 generalParser :: Int -> Parser SExpr
-generalParser _ (')':xs) = trace ("Error : `)` given to generalParser") Nothing
-generalParser line ('\n':xs) = trace ("Info : skip line return") generalParser (line + 1) xs
-generalParser line ('\t':xs) = trace ("Info : skip tab") generalParser line xs
-generalParser line (' ':xs) = trace ("Info : skip spaces") generalParser line xs
+generalParser _ (')':xs) = Nothing
+generalParser line ('\n':xs) = generalParser (line + 1) xs
+generalParser line ('\t':xs) = generalParser line xs
+generalParser line (' ':xs) = generalParser line xs
 generalParser line str = case parseInt str of
     Just (i, is) -> Just ((IntExpr i line), is)
     Nothing -> case parseString line str of
         Just (s, ss) -> Just (s, ss)
         Nothing -> case (parseList line str) of
             Just (list, ls) -> Just (ExprList list, ls)
-            Nothing -> trace ("Fail: general parser failed :c") Nothing
-
---(defun puissance ( base exposant ) \n ( if ( = exposant 0 ) \n 1 \n ( * base ( puissance base ( - exposant 1 ) ) ) ) )
+            Nothing -> Nothing
