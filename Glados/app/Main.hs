f5355edd4a6d93c1dc85c58e33e36cc4543178de
Skaahn <stanislas.commenge@epitech.eu>
2023-12-14 19:31:45 +0100

    feat: clean main calling parser

--- Glados/app/Main.hs
+++ Glados/app/Main.hs
@@ -1,6 +1,16 @@
 module Main (main) where
 
+import Parser (generalParser)
 import Eval
+import Tools (extractFromFile)
+import Error (sendError, errorParams)
+import System.Environment (getArgs)
 
 main :: IO ()
-main = putStrLn $ show $ sexprToAST $ ExprList [StrExpr "define" 1, StrExpr "bob" 1, IntExpr 6 1]
+main = getArgs >>= \args ->
+    case args of
+        [filepath] -> extractFromFile filepath >>= \content ->
+            case generalParser 1 content of
+                Just (expr, _) -> putStrLn $ show expr
+                _ -> sendError 84 "Error: Incomplete parsing"
+        _ -> errorParams
---
+++ Glados/src/Error.hs
@@ -0,0 +1,21 @@
+module Error (sendError, errorParams, exceptionMessage) where
+
+-- IMPORTS
+
+import System.Exit
+import System.IO
+import Control.Exception
+
+-- FUNCTIONS
+
+sendError :: Int -> String -> IO ()
+sendError code message = hPutStrLn stderr message >>
+    exitWith (ExitFailure code)
+
+exceptionMessage :: IOException -> IO String
+exceptionMessage _ = hPutStrLn stderr "Wrong filepath" >>
+    exitWith (ExitFailure 84)
+
+errorParams :: IO ()
+errorParams = hPutStrLn stderr "USAGE: ./glados <filepath>" >>
+    exitWith (ExitFailure 84)
