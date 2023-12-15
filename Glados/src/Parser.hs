ce67bc9d4d12181b1387088509860e37e95941ab
Skaahn <stanislas.commenge@epitech.eu>
2023-12-15 15:25:57 +0100

    chore: fixed warnings

--- Glados/src/Parser.hs
+++ Glados/src/Parser.hs
@@ -1,9 +1,8 @@
-module Parser (generalParser, SExpr(..), exprParser, parseChar, parseMany, parseMany, parseSome, parseInt, parseList, parseString) where
+module Parser (generalParser, SExpr(..), exprParser, parseChar, parseMany, parseSome, parseInt, parseList, parseString) where
 
 -- IMPORTS
 
 import Text.Read
-import Debug.Trace
 
 -- CUSTOM TYPES
 
@@ -17,8 +16,13 @@ data SExpr = IntExpr Int Int -- second int is the line
 
 -- GLOBAL VAR
 
+ascii_char :: [Char]
 ascii_char = enumFromTo ' ' '~'
+
+banned_char :: [Char]
 banned_char = [' ', '(', ')', '\"']
+
+valid_char :: [Char]
 valid_char = filter (`notElem` banned_char) ascii_char
 
 -- FUNCTIONS
@@ -29,26 +33,14 @@ parseChar c (x:xs)
     | otherwise = Nothing
 parseChar _ _ = Nothing
 
-parseAnyChar :: String -> Parser Char
-parseAnyChar (x:xs) str = parseOr (parseChar x) (parseAnyChar xs) str
-parseAnyChar _ _ = Nothing
-
 parseOr :: Parser a -> Parser a -> Parser a
 parseOr p1 p2 s = case p1 s of
     Just (x, xs) -> Just (x, xs)
     Nothing -> p2 s
 
-parseAnd :: Parser a -> Parser b -> Parser (a, b)
-parseAnd p1 p2 s = case p1 s of
-    Just (x, xs) -> case p2 xs of
-        Just (y, ys) -> Just ((x, y), ys)
-        Nothing -> Nothing
-    Nothing -> Nothing
-
-parseAndWith :: (a -> b -> c) -> Parser a -> Parser b -> Parser c
-parseAndWith function p1 p2 s = case parseAnd p1 p2 s of
-    Just ((x, y), xs) -> Just (function x y, xs)
-    Nothing -> Nothing
+parseAnyChar :: String -> Parser Char
+parseAnyChar (x:xs) str = parseOr (parseChar x) (parseAnyChar xs) str
+parseAnyChar _ _ = Nothing
 
 parseMany :: Parser a -> Parser [a]
 parseMany p s = case p s of
@@ -64,14 +56,6 @@ parseSome p s = case p s of
         Nothing -> Just ([x], xs)
     Nothing -> Nothing
 
-parsePair :: Parser a -> Parser (a,a)
-parsePair p ('(':xs) = case (p xs) of
-    Just (a, (' ':as)) -> case (p as) of
-        Just (b, (')':bs)) -> Just ((a, b), bs)
-        _ -> Nothing
-    _ -> Nothing
-parsePair _ _ = Nothing
-
 readUInt :: String -> String -> Maybe (Int, String)
 readUInt s xs = case (readMaybe s :: Maybe Int) of
         Just nb -> Just (nb, xs)
@@ -85,8 +69,6 @@ parseUInt s = case (parseSome(parseAnyChar ['0'..'9']) s) of
     Just (x, ('\t':xs)) -> readUInt x ('\t':xs)
     Just (x, "") -> readUInt x ""
     _ -> Nothing
-    where
-        parsed = parseSome (parseAnyChar ['0'..'9']) s
 
 parseInt :: Parser Int -- parse a signed Int
 parseInt ('-':xs) = case parseUInt xs of
@@ -95,13 +77,13 @@ parseInt ('-':xs) = case parseUInt xs of
 parseInt s = parseUInt s
 
 getBiggerLine :: Int -> SExpr -> Int
-getBiggerLine line (SymbolExpr _ newLine) = newLine
-getBiggerLine line (IntExpr _ newLine) = newLine
-getBiggerLine line (StrExpr _ newLine) = newLine
+getBiggerLine _ (SymbolExpr _ newLine) = newLine
+getBiggerLine _ (IntExpr _ newLine) = newLine
+getBiggerLine _ (StrExpr _ newLine) = newLine
 getBiggerLine line (ExprList _) = line
 
 parseListElement :: Int -> Parser [SExpr]
-parseListElement line (')':xs) = Just ([], xs)
+parseListElement _ (')':xs) = Just ([], xs)
 parseListElement line ('\n':xs) = parseListElement (line + 1) xs
 parseListElement line ('\t':xs) = parseListElement line xs
 parseListElement line (' ':xs) = parseListElement line xs
@@ -114,11 +96,11 @@ parseListElement line str = case generalParser line str of
 
 parseList :: Int -> Parser [SExpr]
 parseList line ('(':xs) = parseListElement line xs
-parseList _ s = Nothing
+parseList _ _ = Nothing
 
 parseString :: Int -> Parser SExpr
-parseString l "" = Nothing
-parseString l (' ':xs) = Nothing
+parseString _ "" = Nothing
+parseString _ (' ':_) = Nothing
 parseString l ('\"':xs) = case parseSome (parseAnyChar (filter (/= '\"') ascii_char)) xs of
     Just (a, '\"':as) -> Just (StrExpr a l, as)
     _ -> Nothing
@@ -136,7 +118,7 @@ exprParser line str = case parseInt str of
             Nothing -> Nothing
 
 generalParser :: Int -> Parser SExpr
-generalParser _ (')':xs) = Nothing
+generalParser _ (')':_) = Nothing
 generalParser line ('\n':xs) = generalParser (line + 1) xs
 generalParser line ('\t':xs) = generalParser line xs
 generalParser line (' ':xs) = generalParser line xs
