1e7de040c0583ff559e2e4bab08a3536d4475183
kepalas02 <arthur.bourdin@epitech.eu>
2024-01-07 15:46:04 +0100

    [ADD] refactor exec functions

--- bootstrap3/main.hs
+++ bootstrap3/main.hs
@@ -10,7 +10,9 @@ type Args = [Value]
 data Value =
     Numerical Int
     | Boolean Bool
+    | Operator Operators
     | Insts [Instruction]
+    | Function Insts
     deriving (Show)
 
 data Operators =
@@ -45,14 +47,14 @@ instance Show Operators where
 data Instruction =
     Push Value
     | Pop
-    | Call Operators
+    | Call
     | Ret
     | JumpIfFalse Int
     | PushArg Int
 instance Show Instruction where
     show (Push x) = "Push " ++ show x
     show (Pop) = "Pop"
-    show (Call x) = "Call " ++ show x
+    show (Call) = "Call"
     show (Ret) = "Ret"
 
 type Stack = [Value]
@@ -72,59 +74,10 @@ exec args ((Push x):xs) stack = exec args xs (x:stack)
 exec args ((Pop):xs) stack = case stack of
     (_:ss) -> exec args xs ss
     _ -> Left "Nothing to pop"
-exec args ((Call Add):xs) stack = case stack of
-    (Numerical x:Numerical y:stack) -> exec args xs ((Numerical (x + y)):stack)
-    (_:_:_) -> Left "Add need Numericals"
-    _ -> Left "Add need two arguments"
-exec args ((Call Sub):xs) stack = case stack of
-    (Numerical x:Numerical y:stack) -> exec args xs ((Numerical (x - y)):stack)
-    (_:_:_) -> Left "Sub need Numericals"
-    _ -> Left "Sub need two arguments"
-exec args ((Call Mul):xs) stack = case stack of
-    (Numerical x:Numerical y:stack) -> exec args xs ((Numerical (x * y)):stack)
-    (_:_:_) -> Left "Mul need Numericals"
-    _ -> Left "Mul need two arguments"
-exec args ((Call Div):xs) stack = case stack of
-    (Numerical _:Numerical 0:_) -> Left "Division by zero"
-    (Numerical x:Numerical y:stack) -> exec args xs ((Numerical (x `div` y)):stack)
-    (_:_:_) -> Left "Div need Numericals"
-    _ -> Left "Div need two arguments"
-exec args ((Call Eq):xs) stack = case stack of
-    (Numerical x:Numerical y:stack) -> exec args xs ((Boolean (x == y)):stack)
-    (_:_:_) -> Left "Eq need Numericals"
-    _ -> Left "Eq need two arguments"
-exec args ((Call Ne):xs) stack = case stack of
-    (Numerical x:Numerical y:stack) -> exec args xs ((Boolean (x /= y)):stack)
-    (_:_:_) -> Left "Ne need Numericals"
-    _ -> Left "Ne need two arguments"
-exec args ((Call Lt):xs) stack = case stack of
-    (Numerical x:Numerical y:stack) -> exec args xs ((Boolean (x < y)):stack)
-    (_:_:_) -> Left "Lt need Numericals"
-    _ -> Left "Lt need two arguments"
-exec args ((Call Gt):xs) stack = case stack of
-    (Numerical x:Numerical y:stack) -> exec args xs ((Boolean (x > y)):stack)
-    (_:_:_) -> Left "Gt need Numericals"
-    _ -> Left "Gt need two arguments"
-exec args ((Call Le):xs) stack = case stack of
-    (Numerical x:Numerical y:stack) -> exec args xs ((Boolean (x <= y)):stack)
-    (_:_:_) -> Left "Le need Numericals"
-    _ -> Left "Le need two arguments"
-exec args ((Call Ge):xs) stack = case stack of
-    (Numerical x:Numerical y:stack) -> exec args xs ((Boolean (x >= y)):stack)
-    (_:_:_) -> Left "Ge need Numericals"
-    _ -> Left "Ge need two arguments"
-exec args ((Call And):xs) stack = case stack of
-    (Boolean x:Boolean y:stack) -> exec args xs ((Boolean (x && y)):stack)
-    (_:_:_) -> Left "And need Booleans"
-    _ -> Left "And need two arguments"
-exec args ((Call Or):xs) stack = case stack of
-    (Boolean x:Boolean y:stack) -> exec args xs ((Boolean (x || y)):stack)
-    (_:_:_) -> Left "Or need Booleans"
-    _ -> Left "Or need two arguments"
-exec args ((Call Not):xs) stack = case stack of
-    (Boolean x:stack) -> exec args xs ((Boolean (not x)):stack)
-    (_:_) -> Left "Not need Booleans"
-    _ -> Left "Not need one argument"
+exec args ((Call):xs) stack = case stack of
+    (Operator op:stack') -> executeOperator args xs op stack'
+    (Function insts:stack') -> exec args insts stack'
+    _ -> Left "Call needs an Operator or Function on the stack"
 exec args ((JumpIfFalse n):xs) stack = case stack of
     (Boolean False:stack) -> if n >= 0 then jumpTo args n xs stack else Left "Negative jumps"
     (Boolean True:stack) -> exec args xs stack
@@ -137,11 +90,63 @@ exec args ((PushArg index):xs) stack = case args of
     _ -> exec args xs ((args !! index) : stack)
 exec _ _ _ = Left "fail"
 
+executeOperator :: Args -> Insts -> Operators -> Stack -> Either String Value
+executeOperator args xs op stack =
+    case op of
+        Add -> executeBinaryOp (\x y -> Right (Numerical (x + y))) args xs stack
+        Sub -> executeBinaryOp (\x y -> Right (Numerical (x - y))) args xs stack
+        Mul -> executeBinaryOp (\x y -> Right (Numerical (x * y))) args xs stack
+        Div -> executeDivOp args xs stack
+        Eq  -> executeBinaryOp (\x y -> Right (Boolean (x == y))) args xs stack
+        Ne  -> executeBinaryOp (\x y -> Right (Boolean (x /= y))) args xs stack
+        Lt  -> executeBinaryOp (\x y -> Right (Boolean (x < y))) args xs stack
+        Gt  -> executeBinaryOp (\x y -> Right (Boolean (x > y))) args xs stack
+        Le  -> executeBinaryOp (\x y -> Right (Boolean (x <= y))) args xs stack
+        Ge  -> executeBinaryOp (\x y -> Right (Boolean (x >= y))) args xs stack
+        And -> executeBooleanOp (&&) args xs stack
+        Or  -> executeBooleanOp (||) args xs stack
+
+executeDivOp :: Args -> Insts -> Stack -> Either String Value
+executeDivOp args xs stack = case stack of
+    (Numerical 0:Numerical _:_) -> Left "Division by zero"
+    (Numerical y:Numerical x:stack') -> exec args xs (Numerical (x `div` y) : stack')
+    _ -> Left "Div operation needs two Numericals"
+
+executeBinaryOp :: (Int -> Int -> Either String Value) -> Args -> Insts -> Stack -> Either String Value
+executeBinaryOp op args xs stack = case stack of
+    (Numerical x:Numerical y:stack') -> op x y >>= \result -> exec args xs (result : stack')
+    _ -> Left "Binary operation needs two Numericals"
+
+executeBooleanOp :: (Bool -> Bool -> Bool) -> Args -> Insts -> Stack -> Either String Value
+executeBooleanOp op args xs stack = case stack of
+    (Boolean x:Boolean y:stack') -> exec args xs (Boolean (op x y) : stack')
+    _ -> Left "Boolean operation needs two Booleans"
+
 main :: IO ()
 main = do
-    let arguments = [Numerical 42]
+    let arguments = [Numerical (-42)]
+    let absCodeFunction =
+            [ PushArg 0
+            , Push (Numerical 0)
+            , Push (Operator Lt)
+            , Call
+            , JumpIfFalse 2
+            , PushArg 0
+            , Ret
+            , PushArg 0
+            , Push (Numerical (-1))
+            , Push (Operator Mul)
+            , Call
+            , Ret
+            ]
+
     let instructions =
-            [ PushArg 0, Push (Numerical 0), Call Le, JumpIfFalse 2, PushArg 0, Ret, PushArg 0, Push (Numerical (-1)), Call Mul, Ret]
+            [ Push (Numerical (-42))
+            , Push (Function absCodeFunction)
+            , Call
+            , Ret
+            ]
+
     case exec arguments instructions [] of
         Left errorMsg -> putStrLn $ "Error: " ++ errorMsg
         Right result -> print result
