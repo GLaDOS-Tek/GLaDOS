f417d929a7adf3e327778d13d0ce6af4358e239e
adambenta <adam.bentaleb@epitech.eu>
2024-01-12 22:28:14 +0100

    feat: working arguments recursive

--- bootstrap3/main.hs
+++ bootstrap3/main.hs
@@ -5,8 +5,6 @@ import Debug.Trace
 
 -- STRUCTS
 
-type Args = [Value]
-
 data Value =
     Numerical Int
     | Boolean Bool
@@ -62,83 +60,89 @@ type Stack = [Value]
 
 type Insts = [Instruction]
 
+type Args = [Value]
+
 type Env = [(String, Value)]
 
 type FuncInsts = [Insts]
 
+type ListArgs = [Args]
+
 -- FUNCTIONS
 
-jumpTo :: Args -> Int -> Insts -> Stack -> Env -> FuncInsts -> Either String Value
-jumpTo args 0 insts stack env funcInsts = exec args insts stack env funcInsts
-jumpTo _ _ [] stack _ _= Left "jumped out of bounds"
-jumpTo args n insts stack env funcInsts= jumpTo args (n - 1) (tail insts) stack env funcInsts
+jumpTo :: Args -> Int -> Insts -> Stack -> Env -> FuncInsts -> ListArgs -> Either String Value
+jumpTo args 0 insts stack env funcInsts listArgs = exec args insts stack env funcInsts listArgs
+jumpTo _ _ [] stack _ _ _= Left "jumped out of bounds"
+jumpTo args n insts stack env funcInsts listArgs = jumpTo args (n - 1) (tail insts) stack env funcInsts listArgs
 
-execFunction :: Args -> Insts -> Stack -> Env -> FuncInsts -> Either String Value
-execFunction args insts stack env funcInsts =
+execFunction :: Args -> Insts -> Stack -> Env -> FuncInsts -> ListArgs -> Either String Value
+execFunction args insts stack env funcInsts listArgs =
     case stack of
-        [] -> exec args insts [] env funcInsts
+        [] -> exec args insts [] env funcInsts listArgs
         (lastValue:restOfStack) ->
-            execFunction (lastValue:args) insts restOfStack env funcInsts
+            execFunction (lastValue : args) insts restOfStack env funcInsts listArgs
 
-exec :: Args -> Insts -> Stack -> Env -> FuncInsts -> Either String Value
-exec args [] (x:xs) _ [] = Right x
-exec args [] [] _ (funcInst:funcInsts) = exec args funcInst [] [] funcInsts
-exec args ((Push x):xs) stack env funcInsts = exec args xs (x:stack) env funcInsts
-exec args (Pop:xs) stack env funcInsts = case stack of
-    (_:ss) -> exec args xs ss env funcInsts
+exec :: Args -> Insts -> Stack -> Env -> FuncInsts -> ListArgs -> Either String Value
+exec args [] (x:xs) _ [] _ = Right x
+exec args [] [] stack (funcInst:funcInsts) (lastArg:listArgs) = exec lastArg funcInst [] stack funcInsts listArgs
+exec args ((Push x):xs) stack env funcInsts listArgs = exec args xs (x:stack) env funcInsts listArgs
+exec args (Pop:xs) stack env funcInsts listArgs = case stack of
+    (_:ss) -> exec args xs ss env funcInsts listArgs
     _ -> Left "Nothing to pop"
-exec args (Call:xs) stack env funcInsts = case stack of
-    (Operator op:stack') -> executeOperator args xs op stack' env funcInsts
-    (Function insts:stack') -> execFunction args insts stack' env (xs : funcInsts)
+exec args (Call:xs) stack env funcInsts listArgs = case stack of
+    (Operator op:stack') -> executeOperator args xs op stack' env funcInsts listArgs
+    (Function insts:stack') -> execFunction args insts stack' env (xs : funcInsts) (args:listArgs)
     _ -> Left "Call needs an Operator or Function on the stack"
-exec args ((JumpIfFalse n):xs) stack env funcInsts = case stack of
-    (Boolean False:stack') -> if n >= 0 then jumpTo args n xs stack' env funcInsts else Left "Negative jumps"
-    (Boolean True:stack') -> exec args xs stack' env funcInsts
+exec args ((JumpIfFalse n):xs) stack env funcInsts listArgs = case stack of
+    (Boolean False:stack') -> if n >= 0 then jumpTo args n xs stack' env funcInsts listArgs else Left "Negative jumps"
+    (Boolean True:stack') -> exec args xs stack' env funcInsts listArgs
     _ -> Left "JumpIfFalse needs Boolean"
-exec args (Ret:_) stack env funcInsts = case funcInsts of
-    [] -> case stack of
-        [] -> Left "No function instance or value to return to"
-        x:_ -> Right x
-    lastFuncInst:restFuncInsts -> exec args lastFuncInst stack env restFuncInsts
-exec args ((PushArg index):xs) stack env funcInsts = case args of
+exec args (Ret:_) stack env funcInsts listArgs =
+    case funcInsts of
+        [] -> case stack of
+            [] -> Left "No function instance or value to return to"
+            x:_ -> Right x
+        lastFuncInst:restFuncInsts ->
+            let (lastArg:restListArgs) = listArgs
+            in exec lastArg lastFuncInst stack env restFuncInsts restListArgs
+exec args ((PushArg index):xs) stack env funcInsts listArgs = case args of
     [] -> Left "No arguments available"
-    _ -> exec args xs ((args !! index) : stack) env funcInsts
-exec args ((PushEnv var):xs) stack env funcInsts = case lookup var env of
-    Just value -> exec args xs (value : stack) env funcInsts
+    _ -> exec args xs ((args !! index) : stack) env funcInsts listArgs
+exec args ((PushEnv var):xs) stack env funcInsts listArgs = case lookup var env of
+    Just value -> exec args xs (value : stack) env funcInsts listArgs
     Nothing -> Left $ "Variable '" ++ var ++ "' not found in environment"
-exec _ _ _ _ _ = Left "fail"
+exec _ _ _ _ _ _ = Left "fail"
 
-
-executeOperator :: Args -> Insts -> Operators -> Stack -> Env -> FuncInsts -> Either String Value
-executeOperator args xs op stack env funcInsts =
+executeOperator :: Args -> Insts -> Operators -> Stack -> Env -> FuncInsts -> ListArgs -> Either String Value
+executeOperator args xs op stack env funcInsts listArgs =
     case op of
-        Add -> executeBinaryOp (\x y -> Right (Numerical (x + y))) args xs stack env funcInsts
-        Sub -> executeBinaryOp (\x y -> Right (Numerical (x - y))) args xs stack env funcInsts
-        Mul -> executeBinaryOp (\x y -> Right (Numerical (x * y))) args xs stack env funcInsts
-        Div -> executeDivOp args xs stack env funcInsts
-        Eq  -> executeBinaryOp (\x y -> Right (Boolean (x == y))) args xs stack env funcInsts
-        Ne  -> executeBinaryOp (\x y -> Right (Boolean (x /= y))) args xs stack env funcInsts
-        Lt  -> executeBinaryOp (\x y -> Right (Boolean (x < y))) args xs stack env funcInsts
-        Gt  -> executeBinaryOp (\x y -> Right (Boolean (x > y))) args xs stack env funcInsts
-        Le  -> executeBinaryOp (\x y -> Right (Boolean (x <= y))) args xs stack env funcInsts
-        Ge  -> executeBinaryOp (\x y -> Right (Boolean (x >= y))) args xs stack env funcInsts
-        And -> executeBooleanOp (&&) args xs stack env funcInsts
-        Or  -> executeBooleanOp (||) args xs stack env funcInsts
+        Add -> executeBinaryOp (\x y -> Right (Numerical (x + y))) args xs stack env funcInsts listArgs
+        Sub -> executeBinaryOp (\x y -> Right (Numerical (x - y))) args xs stack env funcInsts listArgs
+        Mul -> executeBinaryOp (\x y -> Right (Numerical (x * y))) args xs stack env funcInsts listArgs
+        Div -> executeDivOp args xs stack env funcInsts listArgs
+        Eq  -> executeBinaryOp (\x y -> Right (Boolean (x == y))) args xs stack env funcInsts listArgs
+        Ne  -> executeBinaryOp (\x y -> Right (Boolean (x /= y))) args xs stack env funcInsts listArgs
+        Lt  -> executeBinaryOp (\x y -> Right (Boolean (x < y))) args xs stack env funcInsts listArgs
+        Gt  -> executeBinaryOp (\x y -> Right (Boolean (x > y))) args xs stack env funcInsts listArgs
+        Le  -> executeBinaryOp (\x y -> Right (Boolean (x <= y))) args xs stack env funcInsts listArgs
+        Ge  -> executeBinaryOp (\x y -> Right (Boolean (x >= y))) args xs stack env funcInsts listArgs
+        And -> executeBooleanOp (&&) args xs stack env funcInsts listArgs
+        Or  -> executeBooleanOp (||) args xs stack env funcInsts listArgs
 
-executeDivOp :: Args -> Insts -> Stack -> Env -> FuncInsts -> Either String Value
-executeDivOp args xs stack env funcInsts = case stack of
+executeDivOp :: Args -> Insts -> Stack -> Env -> FuncInsts -> ListArgs-> Either String Value
+executeDivOp args xs stack env funcInsts listArgs = case stack of
     (Numerical 0:Numerical _:_) -> Left "Division by zero"
-    (Numerical y:Numerical x:stack') -> exec args xs (Numerical (x `div` y) : stack') env funcInsts
+    (Numerical y:Numerical x:stack') -> exec args xs (Numerical (x `div` y) : stack') env funcInsts listArgs
     _ -> Left "Div operation needs two Numericals"
 
-executeBinaryOp :: (Int -> Int -> Either String Value) -> Args -> Insts -> Stack -> Env -> FuncInsts -> Either String Value
-executeBinaryOp op args xs stack env funcInsts = case stack of
-    (Numerical x:Numerical y:stack') -> op x y >>= \result -> exec args xs (result : stack') env funcInsts
+executeBinaryOp :: (Int -> Int -> Either String Value) -> Args -> Insts -> Stack -> Env -> FuncInsts -> ListArgs -> Either String Value
+executeBinaryOp op args xs stack env funcInsts listArgs = case stack of
+    (Numerical x:Numerical y:stack') -> op x y >>= \result -> exec args xs (result : stack') env funcInsts listArgs
     _ -> Left "Binary operation needs two Numericals"
 
-executeBooleanOp :: (Bool -> Bool -> Bool) -> Args -> Insts -> Stack -> Env -> FuncInsts -> Either String Value
-executeBooleanOp op args xs stack env funcInsts = case stack of
-    (Boolean x:Boolean y:stack') -> exec args xs (Boolean (op x y) : stack') env funcInsts
+executeBooleanOp :: (Bool -> Bool -> Bool) -> Args -> Insts -> Stack -> Env -> FuncInsts -> ListArgs -> Either String Value
+executeBooleanOp op args xs stack env funcInsts listArgs = case stack of
+    (Boolean x:Boolean y:stack') -> exec args xs (Boolean (op x y) : stack') env funcInsts listArgs
     _ -> Left "Boolean operation needs two Booleans"
 
 main :: IO ()
@@ -158,7 +162,7 @@ main = do
              Push (Function incrementFunction),
              Call,
              Ret]
-    
+
     let factFunction =
          [PushArg 0,
          Push (Numerical 1),
@@ -198,14 +202,10 @@ main = do
 
     let instructions =
             [Push (Numerical 5),
-             Push (Numerical 2),
-             PushEnv "fun",
-             Call,
-             Push (Numerical 1),
-             Push (Operator Add),
+             Push (Function factFunction),
              Call,
              Ret]
 
-    case exec [] instructions [] env [] of
+    case exec [] instructions [] env [] [] of
         Left errorMsg -> putStrLn $ "Error: " ++ errorMsg
         Right result -> print result
