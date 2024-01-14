3531a38ca87d3a02c6053b3443d48a916f70f245
Sacha TOPALOFF <sacha.topaloff@epitech.eu>
2024-01-14 16:37:18 +0100

    feat: add VM types to Struct.hs
    - change AST types in Ast.hs according to AstValue in Struct.hs
    - delete useless test file (EvalTests.hs)

--- Glados/src/Ast.hs
+++ Glados/src/Ast.hs
@@ -14,67 +14,76 @@ import Parser
 import Structs
 
 parseNumberAst :: Parser AstValue
-parseNumberAst = Number <$> (parseWS *> parseInt <* parseWS)
+parseNumberAst = AstNumber <$> (parseWS *> parseInt <* parseWS)
 
 parseSymbolAst :: Parser AstValue
-parseSymbolAst = Symbol <$> (parseWS *> parseSymbol <* parseWS)
+parseSymbolAst = AstSymbol <$> (parseWS *> parseSymbol <* parseWS)
 
 parseBooleanAst :: Parser AstValue
-parseBooleanAst = Boolean <$> (parseWS *> parseBoolean <* parseWS)
+parseBooleanAst = AstBoolean <$> (parseWS *> parseBoolean <* parseWS)
 
-parseOperatorAst :: Parser AstValue
-parseOperatorAst = Operator <$> (parseWS *> parseOp <* parseWS)
+parseOperator :: Parser Operator
+parseOperator = parseWS *> parseOp <* parseWS
     where parseOp = Add <$ parseWord "+"
                 <|> Sub <$ parseWord "-"
                 <|> Mul <$ parseWord "*"
                 <|> Div <$ parseWord "/"
-                <|> Equal <$ parseWord "=="
-                <|> NEqual <$ parseWord "!="
+                <|> Eq <$ parseWord "=="
+                <|> Ne <$ parseWord "!="
 
 parseLiteralAst :: Parser AstValue
-parseLiteralAst = Literal <$> (parseWS *> parseString <* parseWS)
+parseLiteralAst = AstLiteral <$> (parseWS *> parseString <* parseWS)
+
+parseOperationAst :: Parser AstValue
+parseOperationAst = do
+    lhs <- parseWS *> parseNumberAst <|> parseSymbolAst
+    op <- parseWS *> parseOperator
+    rhs <- parseWS *> parseAst
+    return (AstBinaryOp op lhs rhs)
 
 parseDefineAst :: Parser AstValue
 parseDefineAst = do
     name <- parseWS *> parseWord "feed" *> parseSymbolAst
     value <- parseWS *> parseAst
-    return (Define name value)
+    return (AstDefine name value)
 
 parseFunctionAst :: Parser AstValue
 parseFunctionAst = do
     name <- parseWS *> parseWord "cat" *> parseSymbolAst
     args <- parseWS *> parseArgListAst
     body <- parseWS *> parseBodyAst
-    return (Func name args body)
+    return (AstFunc name args body)
 
 parseConditionAst :: Parser AstValue
 parseConditionAst = do
     cond <- parseWS *> parseWord "if" *> parseArgListAst
     true <- parseWS *> parseBodyAst
     false <- parseWS *> parseWord "else" *> parseBodyAst
-    return (Cond cond true false)
+    return (AstCond cond true false)
 
 parseCallAst :: Parser AstValue
 parseCallAst = do
     name <- parseWS *> parseSymbolAst
     args <- parseWS *> parseArgListAst
-    return (Call name args)
+    return (AstCall name args)
 
 parseArgListAst :: Parser AstValue
-parseArgListAst = ArgList <$> (parseWS *> parseChar '(' *> parseMany parseAst <* parseChar ')' <* parseWS)
+parseArgListAst = AstArgList <$> (parseWS *> parseChar '(' *> parseMany parseAst <* parseChar ')' <* parseWS)
 
 parseBodyAst :: Parser AstValue
-parseBodyAst = Body <$> (parseWS *> parseChar '{' *> parseMany parseAst <* parseChar '}' <* parseWS)
+parseBodyAst = AstBody <$> (parseWS *> parseChar '{' *> parseMany parseAst <* parseChar '}' <* parseWS)
+
+parseListAst :: Parser AstValue
+parseListAst = AstList <$> (parseWS *> parseChar '(' *> parseMany parseAst <* parseChar ')' <* parseWS)
 
 parseAst :: Parser AstValue
-parseAst = parseBodyAst
-       <|> parseArgListAst
-       <|> parseDefineAst
+parseAst = parseOperationAst
        <|> parseConditionAst
        <|> parseFunctionAst
-       <|> parseNumberAst
-       <|> parseOperatorAst
        <|> parseCallAst
+       <|> parseListAst
+       <|> parseDefineAst
+       <|> parseNumberAst
        <|> parseLiteralAst
        <|> parseBooleanAst
        <|> parseSymbolAst
--- Glados/src/Structs.hs
+++ Glados/src/Structs.hs
@@ -1,34 +1,69 @@
 module Structs (
   Env,
-  BinOp(..),
+  Operator(..),
   AstValue(..)
 ) where
 
 type Env = [(String, AstValue)]
 
-data BinOp =
+type Stack = [Value]
+
+type Insts = [Instruction]
+
+type Args = [Value]
+
+type EnvVm = [(String, Value)]
+
+type FuncInsts = [Insts]
+
+type ListArgs = [Args]
+
+data Operator =
       Add
     | Sub
-    | Div
     | Mul
-    | Greater
-    | Lower
-    | Equal
-    | NEqual
+    | Div
+    | Eq
+    | Ne
+    | Gt
+    | Lt
+    | Le
+    | Ge
+    | And
+    | Or
+    | Not
     deriving (Show, Eq)
 
 data AstValue =
-      Number Int
-    | Literal String
-    | Boolean Bool
-    | Symbol String
-    | List [AstValue]
-    | ArgList [AstValue]
-    | Body [AstValue]
-    | Define AstValue AstValue
-    | Cond AstValue AstValue AstValue
-    | Call AstValue AstValue
-    | Func AstValue AstValue AstValue
-    | Operator BinOp
-    | Error String Int
+      AstNumber Int
+    | AstLiteral String
+    | AstBoolean Bool
+    | AstSymbol String
+    | AstList [AstValue]
+    | AstArgList [AstValue]
+    | AstBody [AstValue]
+    | AstDefine AstValue AstValue
+    | AstCond AstValue AstValue AstValue
+    | AstCall AstValue AstValue
+    | AstFunc AstValue AstValue AstValue
+    | AstBinaryOp Operator AstValue AstValue
+    | AstError String Int
     deriving (Show, Eq)
+
+data Value =
+      Numerical Int
+    | Boolean Bool
+    | Operator Operator
+    | Insts [Instruction]
+    | Function Insts
+    deriving (Show)
+
+data Instruction =
+      Push Value
+    | Pop
+    | Call
+    | Ret
+    | JumpIfFalse Int
+    | PushArg Int
+    | PushEnv String
+    deriving (Show)
